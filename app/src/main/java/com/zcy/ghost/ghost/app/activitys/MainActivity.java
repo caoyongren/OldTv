5665123f2b3317d58d0a3b5a1a5ea5fbdc8db0f1
zjg <2231856598@qq.com>
2016-09-08 16:52:25 +0800

    测试提交zjg

--- app/src/main/java/com/zcy/ghost/ghost/app/activitys/MainActivity.java
+++ app/src/main/java/com/zcy/ghost/ghost/app/activitys/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+    //zjg测试提交
         setTranslucentStatus(true);
         initView();
         initEvent();
