a051a2a3db685d93e2fa9b1f415faf97a6a8242f
岳小川 <759577471@qq.com>
2016-09-08 12:41:32 +0800

    fragment_two

--- app/src/main/java/com/zcy/ghost/ghost/app/fragments/Fragment2.java
+++ app/src/main/java/com/zcy/ghost/ghost/app/fragments/Fragment2.java
@@ -1,30 +1,56 @@
 package com.zcy.ghost.ghost.app.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.decoration.SpaceDecoration;
 import com.zcy.ghost.ghost.R;
+import com.zcy.ghost.ghost.adapter.FoundAdapter;
 import com.zcy.ghost.ghost.app.BaseFragment;
+import com.zcy.ghost.ghost.bean.VideoInfo;
+import com.zcy.ghost.ghost.bean.VideoResult;
+import com.zcy.ghost.ghost.net.Network;
+import com.zcy.ghost.ghost.utils.LogUtils;
+import com.zcy.ghost.ghost.utils.ScreenUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Description:
  * Creator: yxc
  * date: $date $time
  */
-public class Fragment2 extends BaseFragment {
+public class Fragment2 extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
     final String TAG = Fragment2.class.getSimpleName();
 
     @BindView(R.id.title_name)
     TextView titleName;
+    @BindView(R.id.recyclerView)
+    EasyRecyclerView recyclerView;
 
     Unbinder unbinder;
+
+    FoundAdapter adapter;
+    int page = 0;
+    Handler handler = new Handler();
+    VideoResult homeResult;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -37,11 +63,78 @@ public class Fragment2 extends BaseFragment {
     @Override
     protected void initView(LayoutInflater inflater) {
         titleName.setText("发现");
+        recyclerView.setAdapterWithProgress(adapter = new FoundAdapter(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        SpaceDecoration itemDecoration = new SpaceDecoration(ScreenUtil.dip2px(getContext(), 8));
+        itemDecoration.setPaddingEdgeSide(true);
+        itemDecoration.setPaddingStart(true);
+        itemDecoration.setPaddingHeaderFooter(false);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    @Override
+    protected void initEvent() {
+        recyclerView.setRefreshListener(this);
+        onRefresh();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    @Override
+    public void onRefresh() {
+        page = 0;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //刷新
+                getPageHomeInfo();
+            }
+        }, 1000);
+    }
+
+    Observer<VideoResult> observer = new Observer<VideoResult>() {
+        @Override
+        public void onCompleted() {
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Toast.makeText(getActivity(), R.string.loading_failed, Toast.LENGTH_SHORT).show();
+            adapter.stopMore();
+        }
+
+        @Override
+        public void onNext(VideoResult videoResult) {
+            homeResult = videoResult;
+            setAdapter();
+        }
+    };
+
+    private void getPageHomeInfo() {
+        subscription = Network.getVideoApi(getContext())
+                .getHomePage()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
+    }
+
+    private void setAdapter() {
+        if (homeResult != null) {
+            List<VideoInfo> videoInfos = new ArrayList<>();
+            for (int i = 1; i < homeResult.ret.list.size(); i++) {
+                if (!TextUtils.isEmpty(homeResult.ret.list.get(i).title) && !homeResult.ret.list.get(i).title.equals("精彩推荐")) {
+                    VideoInfo videoInfo = homeResult.ret.list.get(i).childList.get(0);
+                    videoInfo.title = homeResult.ret.list.get(i).title;
+                    videoInfos.add(videoInfo);
+                }
+                LogUtils.e(TAG, i + "");
+            }
+            adapter.addAll(videoInfos);
+
+        }
     }
 }
---
+++ app/src/main/java/com/zcy/ghost/ghost/viewholder/FoundViewHolder.java
@@ -0,0 +1,43 @@
+package com.zcy.ghost.ghost.viewholder;
+
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.zcy.ghost.ghost.R;
+import com.zcy.ghost.ghost.bean.VideoInfo;
+
+/**
+ * Created by zhuchenxi on 16/6/2.
+ */
+
+public class FoundViewHolder extends BaseViewHolder<VideoInfo> {
+    ImageView imgPicture;
+    TextView tv_title;
+
+    public FoundViewHolder(ViewGroup parent) {
+        super(parent, R.layout.item_found);
+        imgPicture = $(R.id.img_video);
+        tv_title = $(R.id.tv_title);
+        imgPicture.setScaleType(ImageView.ScaleType.FIT_XY);
+    }
+
+    @Override
+    public void setData(VideoInfo data) {
+        tv_title.setText(data.title);
+        ViewGroup.LayoutParams params = imgPicture.getLayoutParams();
+
+        DisplayMetrics dm = getContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels / 2;//宽度为屏幕宽度一半
+//        int height = data.getHeight()*width/data.getWidth();//计算View的高度
+
+        params.height = (int) (width / 1.8);
+        imgPicture.setLayoutParams(params);
+        Glide.with(getContext())
+                .load(data.pic)
+                .into(imgPicture);
+    }
+}
