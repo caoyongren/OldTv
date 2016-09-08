cd1cdf80d398cc0bff011c4f7f66ae6c25ad83bd
岳小川 <759577471@qq.com>
2016-09-08 11:26:25 +0800

    appname

--- app/src/main/java/com/zcy/ghost/ghost/app/fragments/Fragment3.java
+++ app/src/main/java/com/zcy/ghost/ghost/app/fragments/Fragment3.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zcy.ghost.ghost.R;
 import com.zcy.ghost.ghost.app.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Description:
@@ -14,10 +19,29 @@ import com.zcy.ghost.ghost.app.BaseFragment;
  * date: $date $time
  */
 public class Fragment3 extends BaseFragment {
+    final String TAG = Fragment3.class.getSimpleName();
+
+    @BindView(R.id.title_name)
+    TextView titleName;
+
+    Unbinder unbinder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_three, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        initView(inflater);
         return rootView;
+    }
+
+    @Override
+    protected void initView(LayoutInflater inflater) {
+        titleName.setText("专题");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/zcy/ghost/ghost/net/api/VideoApi.java
+++ app/src/main/java/com/zcy/ghost/ghost/net/api/VideoApi.java
@@ -6,9 +6,11 @@ package com.zcy.ghost.ghost.net.api;
 import com.zcy.ghost.ghost.bean.VideoResult;
 
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import rx.Observable;
 
 public interface VideoApi {
+    @Headers("Cache-Control: public, max-age=3600")
     @GET("homePageApi/homePage.do")
     Observable<VideoResult> getHomePage();
 }
