890515cdebe4ce562c8becba8eebd403b6da7920
zjg <2231856598@qq.com>
2016-09-14 09:45:43 +0800

    更新右滑关闭activity的使用说明注释

--- app/src/main/java/com/zcy/ghost/ghost/app/SwipeBackActivity.java
+++ app/src/main/java/com/zcy/ghost/ghost/app/SwipeBackActivity.java
@@ -11,7 +11,7 @@ import com.zcy.ghost.ghost.views.SwipeBackLayout;
 /**
  * 1、想要实现向右滑动删除Activity效果只需要继承SwipeBackActivity即可，如果当前页面含有ViewPager
  * 只需要调用SwipeBackLayout的setViewPager()方法即可
- *2、
+ *2、设置activity的主题为android:theme="@style/CustomTransparent
  * @author zjg
  *
  */
