1de37cfad670bcc59c8141e7e2fd171945730179
yxc <759577471@qq.com>
2016-09-23 15:25:26 +0800

    历史、收藏

---
+++ app/src/main/java/com/zcy/ghost/vivideo/model/bean/Collection.java
@@ -0,0 +1,12 @@
+package com.zcy.ghost.vivideo.model.bean;
+
+/**
+ * Description: 收藏
+ * Creator: yxc
+ * date: 2016/9/23 11:29
+ */
+public class Collection {
+    public String title;
+    public String pic;
+    public String dataId;
+}
---
+++ app/src/main/java/com/zcy/ghost/vivideo/model/bean/Record.java
@@ -0,0 +1,12 @@
+package com.zcy.ghost.vivideo.model.bean;
+
+/**
+ * Description: 播放记录
+ * Creator: yxc
+ * date: 2016/9/23 11:30
+ */
+public class Record {
+    public String title;
+    public String pic;
+    public String dataId;
+}
