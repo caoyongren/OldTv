e72835c3d3f6a49af5fba5603f386f0acfe046f0
岳兴川 <759577471@qq.com>
2017-09-18 09:08:53 +0800

    add RootView

---
+++ app/src/main/java/com/zcy/ghost/vivideo/base/RootView.java
@@ -0,0 +1,68 @@
+package com.zcy.ghost.vivideo.base;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * Created by yuexingchuan on 17/9/18.
+ */
+public abstract class RootView<T extends BasePresenter> extends View implements BaseView {
+    protected boolean mActive;//是否被销毁
+    protected Context mContext;
+    protected Unbinder unbinder;
+    protected T mPresenter;
+
+    public RootView(Context context) {
+        super(context);
+        init();
+    }
+
+    public RootView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RootView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    protected abstract void getLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initEvent();
+
+    protected void init() {
+        mContext = getContext();
+        getLayout();
+        unbinder = ButterKnife.bind(this);
+        mActive = true;
+        initView();
+        initEvent();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mPresenter != null)
+            mPresenter.attachView(this);
+        mActive = true;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mPresenter != null)
+            mPresenter.detachView();
+        mActive = false;
+        unbinder.unbind();
+        mContext = null;
+    }
+}
+
